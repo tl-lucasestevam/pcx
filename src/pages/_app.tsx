8aa76f07e97d0e12723a9b7d3ad81d170cf5ca98
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-03-11 12:08:34 -0300

    chore: Add chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import theme from "../styles";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
