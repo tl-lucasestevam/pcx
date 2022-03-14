c782ee71e16e59e5dc1720c8bf0e44c2c5ee32e0
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-03-13 22:45:30 -0300

    fix: User name display in header

--- src/components/Header/LoginButton/index.tsx
+++ src/components/Header/LoginButton/index.tsx
@@ -2,13 +2,13 @@ import { FC } from "react";
 import { HStack, Avatar, Text } from "@chakra-ui/react";
 
 const LoginButton: FC = () => {
-  const name = "";
+  const firstName = "";
   const avatarUrl = "";
 
   return (
     <HStack spacing="4" _hover={{ cursor: "pointer" }}>
-      <Text fontWeight="600">{name ? name.split(" ")[0] : "Login"}</Text>
-      <Avatar color="gray.600" bg="gray.200" name={name} src={avatarUrl} />
+      <Text fontWeight="600">{firstName ? firstName : "Login"}</Text>
+      <Avatar color="gray.600" bg="gray.200" name={firstName} src={avatarUrl} />
     </HStack>
   );
 };
