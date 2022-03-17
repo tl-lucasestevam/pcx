c450ed9a649f570caddad548a46bfac6bbe642cc
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-03-17 19:39:27 -0300

    refactor: input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -5,36 +5,55 @@ import {
   InputGroup,
   InputProps as ChakraInputProps,
   InputLeftElement,
+  InputRightElement,
 } from "@chakra-ui/react";
 import { forwardRef, ForwardRefRenderFunction, ReactNode } from "react";
 import { FieldError } from "react-hook-form";
+import InputMask from "react-input-mask";
 
 interface InputProps extends ChakraInputProps {
   name: string;
+  mask?: string;
+  iconLeft?: ReactNode;
+  iconRight?: ReactNode;
   error?: FieldError;
-  icon?: ReactNode;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
-  { name, error = null, icon = null, ...rest },
+  {
+    name,
+    error = null,
+    iconLeft = null,
+    iconRight = null,
+    mask = null,
+    ...rest
+  },
   ref
 ) => {
   return (
     <FormControl isInvalid={!!error}>
       <InputGroup>
-        {!!icon && (
-          <InputLeftElement pointerEvents="none">{icon}</InputLeftElement>
+        {iconLeft && (
+          <InputLeftElement pointerEvents="none">{iconLeft}</InputLeftElement>
         )}
         <ChakraInput
           paddingInlineStart="5.25rem"
-          borderColor={!!error ? "red.500" : "inherit"}
+          borderColor={error ? "red.500" : "inherit"}
           name={name}
           id={name}
           ref={ref}
+          as={mask ? InputMask : ChakraInput}
+          mask={mask}
+          maskChar={null}
           {...rest}
         />
+        {iconRight && (
+          <InputRightElement pointerEvents="none">
+            {iconRight}
+          </InputRightElement>
+        )}
       </InputGroup>
-      {!!error && (
+      {error && (
         <FormErrorMessage justifyContent="center" mt="2">
           {error.message}
         </FormErrorMessage>
